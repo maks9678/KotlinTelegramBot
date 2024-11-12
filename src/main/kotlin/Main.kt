377a7805d12ee234e7166b689295b1fa1c97185b
Maksim Medvedev <maksim.9678@mail.ru>
2024-11-12 22:57:13 +0300

    KTB-02 forEachLine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,11 +4,5 @@ import java.io.File
 
 fun main() {
     val dictionary = File("word.txt")
-    dictionary.writeText("hello привет\n")
-    dictionary.appendText("cat  кот\n")
-    dictionary.appendText("dog собака\n")
-
-    val listWord = dictionary.readLines()
-
-    listWord.forEach { println(it) }
+    dictionary.forEachLine {println(it)}
 }
