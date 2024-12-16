9b399baa680018aa160ada9e3964a15f3bf4d8e3
Maksim Medvedev <maksim.9678@mail.ru>
2024-12-16 23:05:26 +0300

    textUpdateId

--- src/main/kotlin/Telegram,.kt
+++ src/main/kotlin/Telegram,.kt
@@ -3,20 +3,32 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+
 fun main(args: Array<String>) {
 
     val botToken = args[0]
     var updateId = 0
     while (true) {
         Thread.sleep(2000)
+        try {
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updatesString = updates.substring(startUpdateId + 11, endUpdateId)
 
-        updateId = updatesString.toInt() + 1
+
+        val messageUpdateIdRegex: Regex = "\"update_id\":\"(.+?)\"".toRegex()
+        val mathResultUpdateId: kotlin.text.MatchResult? = messageUpdateIdRegex.find(updates)
+        val groupsUpdateId = mathResultUpdateId?.groups
+        val textUpdateId = groupsUpdateId?.get(1)?.value
+            updateId = textUpdateId?.toInt()?.plus(1) ?: continue
+
+            val messageInputTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+            val mathResultInputText: kotlin.text.MatchResult? = messageInputTextRegex.find(updates)
+            val groupsInputText = mathResultInputText?.groups
+            val inputText = groupsInputText?.get(1)?.value
+            println(inputText)
+        }catch (e:Exception){continue}
+
+
     }
 }
 
