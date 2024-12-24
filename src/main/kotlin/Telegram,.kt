8dbca1cef65756bcc9b9cd384fbed77f01558f0d
maksim <maksim.9678@mail.ru>
2024-12-24 23:41:59 +0300

    refactor 2

--- src/main/kotlin/Telegram,.kt
+++ src/main/kotlin/Telegram,.kt
@@ -4,6 +4,7 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+const val URL_BOT = "https://api.telegram.org/bot"
 
 fun main(args: Array<String>) {
     var text = "Hello"
@@ -11,40 +12,38 @@ fun main(args: Array<String>) {
     while (true) {
         Thread.sleep(2000)
         val updates: String = telegramBot.getUpdates()
+        val messageUpdateIdRegex: Regex = "\"update_id\":(\\d+)".toRegex()
+        val messageInputTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
+        val messageChatIdRegex: Regex = "\"chat\"\\s*\\{[^}]*\"id\":\\s*(\\d+)".toRegex()
 
         telegramBot.updateId =
-            telegramBot.messageUpdateIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull()?.plus(1) ?: continue
+            messageUpdateIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull()?.plus(1) ?: continue
         println(updates)
 
-        val inputText = telegramBot.messageInputTextRegex.find(updates)?.groups?.get(1)?.value ?: continue
+        val inputText = messageInputTextRegex.find(updates)?.groups?.get(1)?.value ?: continue
         println(inputText)
 
-        val chatId = telegramBot.messageChatIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull() ?: continue
+        val chatId = messageChatIdRegex.find(updates)?.groups?.get(1)?.value?.toIntOrNull() ?: continue
+
         if (inputText != text) {
             text = "Введите другое слово"
         } else println(telegramBot.sendMessage(chatId))
     }
 }
 
-class TelegramBotService(args: String) {
-    private val botToken = args
+class TelegramBotService(private val botToken: String) {
     var updateId = 0
-    val messageUpdateIdRegex: Regex = "\"update_id\":(\\d+)".toRegex()
-    val messageInputTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
-    val messageChatIdRegex: Regex = "\"chat\"\\s*\\{[^}]*\"id\":\\s*(\\d+)".toRegex()
-
-
     private val client: HttpClient = HttpClient.newBuilder().build()
-    fun getUpdates(): String {
 
-        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+    fun getUpdates(): String {
+        val urlGetUpdates = "$URL_BOT$botToken/getUpdates?offset=$updateId"
         val requestUpdates = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
         val responseUpdates = client.send(requestUpdates, HttpResponse.BodyHandlers.ofString())
         return responseUpdates.body()
     }
 
     fun sendMessage(chatId: Int): String {
-        val urlOutput = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$text"
+        val urlOutput = "$URL_BOT$botToken/sendMessage?chat_id=$chatId&text=$text"
         val requestUpdates = HttpRequest.newBuilder().uri(URI.create(urlOutput)).build()
         val responseUpdates = client.send(requestUpdates, HttpResponse.BodyHandlers.ofString())
         return responseUpdates.body()
